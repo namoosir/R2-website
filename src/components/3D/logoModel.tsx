733da6ae38c1b81bc6fcb1713f7842ffe4b007d9
t32rahman <tamimr247@gmail.com>
2024-02-14 00:35:55 -0500

    model param update

--- src/components/3D/logoModel.tsx
+++ src/components/3D/logoModel.tsx
@@ -1,37 +1,56 @@
-import { RefObject, useEffect, useRef } from "react";
+import * as THREE from "three";
+import { RefObject, useEffect, useRef, useState } from "react";
 import { useGLTF } from "@react-three/drei";
-import { SpringOptions, useMotionValue, useSpring, useScroll, useTransform } from "framer-motion";
+import { SpringOptions, useMotionValue, useScroll, useTransform, useSpring } from "framer-motion";
 import { motion } from 'framer-motion-3d';
 import { MeshProps } from "@react-three/fiber";
+import { GLTF } from "three-stdlib";
 
 type LogoModelProps = {
     containerRef: RefObject<HTMLDivElement>;
 }
 
+type GLTFResult = GLTF & {
+    nodes: {
+        Text: THREE.Mesh;
+    };
+    materials: {
+        ["Material.007"]: THREE.MeshStandardMaterial;
+    };
+};
+
 export default function LogoModel(props: LogoModelProps) {
-    const { nodes, materials } = useGLTF("/R2_Logo_final.glb");
+    const { nodes, materials } = useGLTF("/R2_Logo_final.glb") as GLTFResult;
     const modelRef = useRef<MeshProps>(null);
+    const [mousePosition, setMousePosition] = useState({ x: + 0.25, y: Math.PI / 2 - 0.25 });
+
     const options: SpringOptions = {
-        damping: 90
+        damping: 10,
+        stiffness: 300,
     };
 
     //mouse stuff
     const mouse = {
         x: useSpring(useMotionValue(0), options),
-        y: useSpring(useMotionValue(Math.PI / 2), options)
+        y: useSpring(useMotionValue(0), options)
     };
+
     const manageMouseMove = (e: { clientX: any; clientY: any; }) => {
-        const { innerWidth, innerHeight } = window;
+        let { innerWidth, innerHeight } = window;
         const { clientX, clientY } = e;
 
-        const x = -1*(-0.5 + clientX / innerWidth);
-        const y = -0.5 + clientY / innerHeight + Math.PI / 2;
+        const x = -1 * (-0.5 + (clientX / innerWidth));
+        const y = -0.5 + (clientY / innerHeight) + (Math.PI / 2);
 
-        mouse.x.set(x);
-        mouse.y.set(y);
+        setMousePosition({ x, y });
     }
 
-    //scroll stuff
+    useEffect(() => {
+        mouse.x.set(mousePosition.x);
+        mouse.y.set(mousePosition.y);
+    }, [mousePosition]);
+
+    // scroll stuff
     const { scrollYProgress } = useScroll({
         target: props.containerRef,
         offset: ['start 72px', 'center start']
@@ -48,12 +67,14 @@ export default function LogoModel(props: LogoModelProps) {
             modelRef.current.geometry.center();
         }
 
-        window.addEventListener("mousemove", manageMouseMove);
+        setTimeout(() => {
+            window.addEventListener("mousemove", manageMouseMove);
+        }, 500);
 
         return () => {
             window.removeEventListener("mousemove", manageMouseMove);
         }
-    }, []);    
+    }, []);
 
     return (
         <group dispose={null}>
@@ -66,8 +87,9 @@ export default function LogoModel(props: LogoModelProps) {
 
                 // only scroll animation
                 // rotation-z={smoothProgress}
-                
+
                 rotation-z={combinedZRotation}
+
                 castShadow
                 receiveShadow
                 geometry={nodes.Text.geometry}
